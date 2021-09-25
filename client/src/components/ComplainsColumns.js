0f83a9ebb0b3c5e20e2f0fcad37eed5809abbb02
Sinadin Shan <sinadinshan66@gmail.com>
2021-09-26 02:09:08 +0530

    Added icons

--- client/src/components/ComplainsColumns.js
+++ client/src/components/ComplainsColumns.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Cards from './Card'
+import {FaRegDotCircle,FaRegCheckCircle,FaRegArrowAltCircleUp} from 'react-icons/fa'
 
 const ComplainsColumns = (props) => {  //This generates each of the three columns. Props passes are status (0, 1 or 2) and the list of all complaints (list of dictionaries)
     const statusName = props.status==0?'Open':props.status==1?'Assigned':'Closed'
@@ -8,7 +9,7 @@ const ComplainsColumns = (props) => {  //This generates each of the three column
         <div className={`col col${props.status}`}>
             
                 <div className={`${statusName+'ComplaintsTitle'} complaintsTitle`}>
-                    {props.status==0?'Open':props.status==1?'Assigned':'Closed'}
+                    {props.status==0?<span><FaRegDotCircle />Open</span>:props.status==1?<span><FaRegArrowAltCircleUp />Assigned</span>:<span><FaRegCheckCircle />Closed</span>}
                 </div>
                 
                 {
