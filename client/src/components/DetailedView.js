53fc8c529f050c6aeec21eca55b6dc84df775642
Jawadtp <jawadtpmohd@gmail.com>
2021-09-25 23:05:55 +0530

    Detailed view template setup

--- client/src/components/DetailedView.js
+++ client/src/components/DetailedView.js
@@ -1,14 +1,60 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const DetailedView = (props) => 
 {
-    fetch('http://localhost:5000/complaints/'+props.id)
+    
+    const [complaint, setComplaintInfo] = useState({})
+
+
+    function onComplaintInfoLoad(data)
+    {
+        setComplaintInfo(data)
+        console.log(data.complaint)
+    }
+
+    useEffect(() => 
+    {
+        fetch('http://localhost:5000/complaints/'+props.id)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => onComplaintInfoLoad(data));
+
+    },[]);
+
 
     return (
-        <div>
-            Detailed view {props.id}
+        <div className="detailViewPageWrapper">
+            {Object.keys(complaint).length==0?'Loading':
+            <div className="detailedViewWrapper">
+                <div className="authorName">
+                    Reported by: {complaint.complaint.author}
+                  <span className="issueDate">{complaint.complaint.date}</span>
+                </div>
+
+                <div className="titleHeader">
+                    Title
+                </div>
+
+                <div className="issueTitle">
+                    {complaint.complaint.title}
+                </div>
+
+                <div className="descHeader">
+                    Description
+                </div>
+
+                <div className="issueDesc">
+                    {complaint.complaint.description}
+                </div>
+                <div className="votesWrapper">
+                    <div className="votesTitle">
+                        Upvotes
+                    </div>
+                    <div className="votes">
+                        {complaint.complaint.votes}
+                    </div>
+                </div>
+            </div>
+            }
         </div>
     )
 }
