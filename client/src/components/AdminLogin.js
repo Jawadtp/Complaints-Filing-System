acbbb339eeb58acbc034dc6a4c4b1510623b034b
Sinadin Shan <sinadinshan66@gmail.com>
2021-09-25 22:34:05 +0530

    Login page completed

--- client/src/components/AdminLogin.js
+++ client/src/components/AdminLogin.js
@@ -1,7 +1,18 @@
+import '../login.css'
 
 function AdminLogin(){
     return(
-        <h1>Adminey, Pooy</h1>
+        <div class="wrapper adminLogin">
+            <div id="formContent">
+                
+                <h2 class="active"> Sign In </h2>
+                <form>
+                    <input type="text" id="login" class="second" name="login" placeholder="login" />
+                    <input type="text" id="password" class="third" name="login" placeholder="password" />
+                    <input type="submit" class="fourth" value="Log In" />
+                </form>
+            </div>
+        </div>
     )
 }
 
